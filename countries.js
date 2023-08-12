5deb345a46dc9d216498e6438d939dff8403f97c
HossamDaCyborg <hossamhamdy1482002@gmail.com>
2023-08-12 21:01:11 +0300

    not much

---
+++ countries.js
@@ -0,0 +1,4 @@
+
+let ls =
+  " Portugal-- Lithuania-- United Kingdom-- Canada-- United Arab Emirates-- Croatia-- France-- Qatar-- Ireland-- Belgium-- Singapore-- Slovakia-- Latvia-- Saudi Arabia-- Cyprus-- Israel-- Italy-- Taiwan-- Australia-- Hungary-- South Africa-- Puerto Rico-- Romania-- Bulgaria-- Bosnia and Herzegovina-- South Korea-- Mexico-- Turkey-- Uruguay-- Costa Rica-- Georgia-- Ecuador-- Malaysia-- Kuwait-- Serbia-- Tunisia-- Panama-- Greece-- Ukraine-- India-- North Macedonia-- Malta-- Belarus-- Morocco-- Brazil-- Argentina-- China-- Azerbaijan-- Colombia-- Russia-- Pakistan-- Thailand-- Chile-- Lebanon-- Hong Kong-- Kazakhstan-- Kenya-- Indonesia-- Vietnam-- Egypt-- Philippines-- Peru-- Venezuela-- Sri Lanka-- Bangladesh-- Iran-- Jordan";
+export let lg = ls.split("--");
